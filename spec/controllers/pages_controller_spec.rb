71fdfe9cc80adb5d848a90f423a3a722d021b6d8
jasonalles <jalles@emme-inc.com>
2012-11-06 07:56:25 -0800

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -25,7 +25,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-        :content => "Ruby on Rails Tutorial Sample | Contact")
+        :content => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 
@@ -38,7 +38,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-        :content => "Ruby on Rails Tutorial Sample | Contact")
+        :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end
 
